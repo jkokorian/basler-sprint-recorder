7637042899ebb71f84c9828000b66bcafb1165d5
jkokorian <jkokorian@gmail.com>
2016-05-03 14:51:52 +0200

    Initial commit with existing labview files

---
+++ test_client.py
@@ -0,0 +1,16 @@
+import zmq
+
+from tinyrpc.protocols.jsonrpc import JSONRPCProtocol
+from tinyrpc.transports.zmq import ZmqClientTransport
+from tinyrpc import RPCClient
+import numpy as np
+import time
+
+ctx = zmq.Context()
+
+rpc_client = RPCClient(
+    JSONRPCProtocol(),
+    ZmqClientTransport.create(ctx, 'tcp://127.0.0.1:10101')
+)
+
+basler = rpc_client.get_proxy()
